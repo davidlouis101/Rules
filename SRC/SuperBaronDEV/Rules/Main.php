11b0ab8911f6c16807716030ee3e45107a8f4157
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-01-31 19:31:28 +0100

    Update Main.php

--- SRC/SuperBaronDEV/Rules/Main.php
+++ SRC/SuperBaronDEV/Rules/Main.php
@@ -6,8 +6,8 @@ use pocketmine\command\ComamndSender;
 class Rules expands PluginBase{
 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     $commandName = $command->getName();
-    if($commandName === "rules"){
-      $sender->sendMessage("§eRules is§6-§71.don't spam    2.don't grief    §73.don't write bad words    4.don't ask for op");
+    if($commandName === "Regeln"){
+      $sender->sendMessage("§eRegeln§6-§71.Nicht Spamen    2.Nicht Gri    §73.Schreibe keine Bosen Worter    4.Frage nicht nach Op");
       return false;
     }
     return false;
